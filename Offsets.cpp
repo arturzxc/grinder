725abcd25796d1a64d20fd8b5494ea0298b323b5
Artur K <arturzxc@hotmail.com>
2023-10-05 21:59:22 +0100

    update

--- Offsets.cpp
+++ Offsets.cpp
@@ -1,15 +1,15 @@
-//GameVersion=v3.0.45.39
-//LastUpdated=04/10/2023
+//GameVersion=v3.0.46.46
+//LastUpdated=05/10/2023
 //UpdatedBy:arturzxc
 #pragma once
 // core    
 constexpr long OFF_REGION = 0x140000000;                      //[Static]->Region
-constexpr long OFF_LEVEL = 0x16f61c0;                         //[Miscellaneous]->LevelName
-constexpr long OFF_LOCAL_PLAYER = 0x21d2738;                  //[Miscellaneous]->LocalPlayer
-constexpr long OFF_ENTITY_LIST = 0x1e223f8;                   //[Miscellaneous]->cl_entitylist
+constexpr long OFF_LEVEL = 0x16f51c0;                         //[Miscellaneous]->LevelName
+constexpr long OFF_LOCAL_PLAYER = 0x21d1738;                  //[Miscellaneous]->LocalPlayer
+constexpr long OFF_ENTITY_LIST = 0x1e213f8;                   //[Miscellaneous]->cl_entitylist
 // player
 constexpr long OFF_ZOOMING = 0x1b91;                          //[RecvTable.DT_Player]->m_bZooming
-constexpr long OFF_LOCAL_ORIGIN = 0x017c;                     //[DataMap.CBaseViewModel]->m_localOrigin
+constexpr long OFF_LOCAL_ORIGIN = 0x0188;                     //[DataMap.CBaseViewModel]->m_localOrigin
 constexpr long OFF_TEAM_NUMBER = 0x037c;                      //[RecvTable.DT_BaseEntity]->m_iTeamNum
 constexpr long OFF_NAME = 0x04b9;                             //[RecvTable.DT_BaseEntity]->m_iName
 constexpr long OFF_SIGNIFIER_NAME = 0x04b0;                   //[RecvTable.DT_BaseEntity]->m_iSignifierName
@@ -26,6 +26,6 @@ constexpr long OFF_WEAPON_INDEX = 0x1798;                     //[RecvTable.DT_We
 constexpr long OFF_GLOW_ENABLE = 0x294;                       //[DT_HighlightSettings].?
 constexpr long OFF_GLOW_THROUGH_WALL = 0x278;                 //[DT_HighlightSettings].?
 constexpr long OFF_GLOW_HIGHLIGHT_ID = 0x298;                 //[DT_HighlightSettings].m_highlightServerActiveStates    
-constexpr long OFF_GLOW_HIGHLIGHTS = 0xB5F8620;               //[?]->?
+constexpr long OFF_GLOW_HIGHLIGHTS = 0xB5F7620;               //[?]->?
 
 
