3f875d68069c8d8cba287bfcbbc51eafa37cd036
Artur K <arturzxc@hotmail.com>
2023-10-12 20:11:29 +0100

    minor

--- AimBot.cpp
+++ AimBot.cpp
@@ -6,8 +6,9 @@ struct AimBot {
     std::vector<Player*>* players;
     Player* target = nullptr;
 
-    const float maxDegreesFOV = 0.5;
-    const int maxDistance = util::metersToGameUnits(60);
+    const int STICK_SPEED = 20;
+    const float MAX_FOV = 0.5;
+    const int MAX_DISTANCE = util::metersToGameUnits(60);
 
     AimBot(XDisplay* display, Level* level, LocalPlayer* localPlayer, std::vector<Player*>* players) {
         this->display = display;
@@ -25,18 +26,16 @@ struct AimBot {
         if (target == nullptr) assignTarget();
         if (target == nullptr) return;
         if (!target->visible) return;
-        if (target->distance2DToLocalPlayer > maxDistance) { target = nullptr; return; };
-
-        int stickSpeed = 20;
+        if (target->distance2DToLocalPlayer > MAX_DISTANCE) { target = nullptr; return; };
 
         //No recoil
         FloatVector2D punchAnglesDiff = localPlayer->punchAnglesDiff;
-        int nrPitchIncrement = floor(punchAnglesDiff.x * stickSpeed);
-        int nrYawIncrement = floor(-punchAnglesDiff.y * stickSpeed);
+        int nrPitchIncrement = floor(punchAnglesDiff.x * STICK_SPEED);
+        int nrYawIncrement = floor(-punchAnglesDiff.y * STICK_SPEED);
 
         //Aimbot
-        int aimYawIncrement = floor(target->aimbotDesiredAnglesIncrement.y * stickSpeed * -1);
-        int aimPitchIncrement = floor(target->aimbotDesiredAnglesIncrement.x * stickSpeed * 1);
+        int aimYawIncrement = floor(target->aimbotDesiredAnglesIncrement.y * STICK_SPEED * -1);
+        int aimPitchIncrement = floor(target->aimbotDesiredAnglesIncrement.x * STICK_SPEED * 1);
 
         //move stick
         display->moveControllerAimStick(
@@ -51,8 +50,8 @@ struct AimBot {
             if (!p->enemy) continue;
             if (!p->visible) continue;
             if (p->aimedAt) continue;
-            if (fabs(p->aimbotDesiredAnglesIncrement.x) > maxDegreesFOV) continue;
-            if (fabs(p->aimbotDesiredAnglesIncrement.y) > maxDegreesFOV) continue;
+            if (fabs(p->aimbotDesiredAnglesIncrement.x) > MAX_FOV) continue;
+            if (fabs(p->aimbotDesiredAnglesIncrement.y) > MAX_FOV) continue;
             if (target == nullptr || p->aimbotScore > target->aimbotScore) target = p;
         }
     }
