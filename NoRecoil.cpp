2d1660f436902fb44994493b208e48ca9a57e77f
tst0 <97851074+tst0@users.noreply.github.com>
2023-11-20 23:41:38 +0300

    Update NoRecoil.cpp
    Updated offsets for November 20 2023 update.

--- NoRecoil.cpp
+++ NoRecoil.cpp
@@ -1,35 +1,36 @@
+//LastUpdated=20/11/2023
 #pragma once
-struct NoRecoil {
-    ConfigLoader* cl;
-    XDisplay* display;
-    Level* level;
-    LocalPlayer* localPlayer;
-
-    FloatVector2D previous_weaponPunchAngles;
-
-    NoRecoil(ConfigLoader* cl, XDisplay* display, Level* level, LocalPlayer* localPlayer) {
-        this->cl = cl;
-        this->display = display;
-        this->level = level;
-        this->localPlayer = localPlayer;
-    }
-
-    void controlWeapon(int counter) {
-        if (!cl->FEATURE_NORECOIL_ON)return;
-        if (!localPlayer->isCombatReady()) return;
-        if (!localPlayer->inAttack) return;
-        FloatVector2D punchAnglesDiff = localPlayer->punchAnglesDiff;
-        if (punchAnglesDiff.isZeroVector()) return;
-        int pitch = (punchAnglesDiff.x > 0)
-            ? roundHalfEven(punchAnglesDiff.x * cl->NORECOIL_PITCH_REDUCTION)
-            : 0;
-        int yaw = roundHalfEven(-punchAnglesDiff.y * cl->NORECOIL_YAW_REDUCTION);
-        display->moveMouseRelative(pitch, yaw);
-    }
-
-    int roundHalfEven(float x) {
-        return (x >= 0.0)
-            ? static_cast<int>(std::round(x))
-            : static_cast<int>(std::round(-x)) * -1;
-    }
-};
+ 
+//core
+constexpr long OFF_REGION = 0x140000000;                      //[Static]->Region
+constexpr long OFF_LEVEL = 0x16966f0;                         //[Miscellaneous]->LevelName
+constexpr long OFF_LOCAL_PLAYER = 0x211fac8;                  //[Miscellaneous]->LocalPlayer
+constexpr long OFF_ENTITY_LIST = 0x1d71858;                   //[Miscellaneous]->cl_entitylist
+    
+//buttons
+constexpr long OFF_IN_ATTACK = 0x0738af90;                     //[Buttons]->in_attack
+constexpr long OFF_IN_JUMP = 0x0738b0a0;                       //[Buttons]->in_jump
+    
+// player
+constexpr long OFF_ZOOMING = 0x1c01;                          //[RecvTable.DT_Player]->m_bZooming
+constexpr long OFF_LOCAL_ORIGIN = 0x0188;                     //[DataMap.CBaseViewModel]->m_localOrigin
+constexpr long OFF_TEAM_NUMBER = 0x037c;                      //[RecvTable.DT_BaseEntity]->m_iTeamNum
+constexpr long OFF_CURRENT_HEALTH = 0x036c;                   //[RecvTable.DT_BaseEntity]->m_iHealth
+constexpr long OFF_CURRENT_SHIELDS = 0x01a0;                  //[RecvTable.DT_BaseEntity]->m_shieldHealth
+constexpr long OFF_NAME = 0x04b9;                             //[RecvTable.DT_BaseEntity]->m_iName
+constexpr long OFF_SIGNIFIER_NAME = 0x04b0;                   //[RecvTable.DT_BaseEntity]->m_iSignifierName
+constexpr long OFF_LIFE_STATE = 0x06c8;                       //[RecvTable.DT_Player]->m_lifeState
+constexpr long OFF_BLEEDOUT_STATE = 0x2710;                   //[RecvTable.DT_Player]->m_bleedoutState
+constexpr long OFF_LAST_VISIBLE_TIME = 0x19bd + 0x3;          //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3
+constexpr long OFF_LAST_AIMEDAT_TIME = 0x19bd + 0x3 + 0x8;    //[RecvTable.DT_BaseCombatCharacter]->m_hudInfo_visibilityTestAlwaysPasses + 0x3 + 0x8
+constexpr long OFF_VIEW_ANGLES = 0x2564 - 0x14;               //[DataMap.C_Player]-> m_ammoPoolCapacity - 0x14
+constexpr long OFF_PUNCH_ANGLES = 0x2468;                     //[?]->m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
+//weapon
+constexpr long OFF_WEAPON_HANDLE = 0x1964;                    //[RecvTable.DT_Player]->m_latestPrimaryWeapons
+constexpr long OFF_WEAPON_INDEX = 0x17a8;                     //[RecvTable.DT_WeaponX]->m_weaponNameIndex
+//glow
+constexpr long OFF_GLOW_ENABLE = 0x294;                       //[DT_HighlightSettings].?
+constexpr long OFF_GLOW_THROUGH_WALL = 0x278;                 //[DT_HighlightSettings].?
+constexpr long OFF_GLOW_FIX = 0x270;
+constexpr long OFF_GLOW_HIGHLIGHT_ID = 0x298;                 //[DT_HighlightSettings].m_highlightServerActiveStates
+constexpr long OFF_GLOW_HIGHLIGHTS = 0xb5cc530;
